8323e80f2cc640014f77d6df5adcf201945f9f6e
Ehren <ehren.chen.ej@gmail.com>
2023-03-20 00:15:11 +0800

    add Fake data: add Factory and databaseSeed to feed the data to db

---
+++ database/factories/AnimalFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Animal;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Animal>
+ */
+class AnimalFactory extends Factory
+{
+    protected $model = Animal::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            // numberBetween 隨機產生範圍1到3之間的整數
+            'type_id'      => $this->faker->numberBetween(1, 3),
+            'name'         => $this->faker->name, // 隨機名稱
+            'birthday'     => $this->faker->date(), // 隨機日期
+            'area'         => $this->faker->city, // 隨機城市名稱
+            'fix'          => $this->faker->boolean, // 隨機布林值
+            'description'  => $this->faker->text, // 隨機一段內容
+            'personality' => $this->faker->text, // 隨機一段內容
+            'user_id'      => User::all()->random()->id, // 隨機綁定一位會員
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Animal;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // 取消Foreign key約束
+        Schema::disableForeignKeyConstraints();
+        Animal::truncate(); // 清空animal資料表 ID歸零
+        User::truncate(); // 清空user資料表 ID歸零
+
+        // 建立5筆會員測試資料
+        User::factory(5)->create();
+        // 建立1筆動物測試資料
+        Animal::factory(10000)->create();
+        // 開啟Foregin Key約束
+        Schema::enableForeignKeyConstraints();
     }
 }
