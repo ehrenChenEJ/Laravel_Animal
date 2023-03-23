9ebdc80ce07fe667467ea360405ceddd04710408
Ehren <ehren.chen.ej@gmail.com>
2023-03-24 02:13:42 +0800

    add Migration content of TYPE

--- database/migrations/2023_03_23_175444_create_types_table.php
+++ database/migrations/2023_03_23_175444_create_types_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique()->comment('類別名稱');
+            $table->integer('sort')->default(100)->comment('排序');
             $table->timestamps();
+            /**
+             * comment() 欄位加上註解
+             * uniquue() 必須是資料表唯一值
+             */
         });
     }
 
