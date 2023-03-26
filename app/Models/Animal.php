1197c441f4a4550c0f48b5baf633deeeb8185c5a
Ehren <ehren.chen.ej@gmail.com>
2023-03-26 22:40:21 +0800

    Add Feature: Model,Build one to many relationship

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -26,4 +26,16 @@ class Animal extends Model
         'personality',
         'user_id', // 不建議允許批量寫入，後續再更動
     ];
+
+    /**
+     * 取得動物分類
+     * 
+     * 命名用單數，因為一個動物只會有一種分類
+     */
+    public function type()
+    {
+        // belongsTo(類別名稱,參照欄位,主鍵)
+        // 因為使用laravel預設的type_id主鍵預設為id，所以後面可以省略
+        return $this->belongsTo('App\Models\Type');
+    }
 }
